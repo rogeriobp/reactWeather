c1d00dfeecc7a34dd0fafdf399e3734115b21713
rogeriobp <rogeriobp@gmail.com>
2016-09-27 17:55:24 -0300

    Styled about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,10 +1,33 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var About = () => {
   return(
     <div>
-      <h3>About</h3>
-      <p>Welcome to the about page!</p>
+      <h1 className="text-center">About</h1>
+      <p>React weather app is a webapp built using React and Foundation as a project for the React Web App Developer course by Andrew Mead.</p>
+      <p>It fetches data from Open Weather Map API, returning the current weather of a city informed by the user.</p>
+      <p>You can find the related links below:</p>
+      <ul>
+        <li>
+          <a href='https://www.udemy.com/the-complete-react-web-app-developer-course/learn/v4/overview'>The Complete React Web App Developer Course - by Andrew Mead</a>
+        </li>
+        <li>
+          <a href='https://facebook.github.io/react/'>React Web Page - JS Library</a>
+        </li>
+        <li>
+          <a href='http://foundation.zurb.com/'>Foundation Web Page - CSS Framework</a>
+        </li>
+        <li>
+          <a href='https://github.com/'>GitHub - Repository</a>
+        </li>
+        <li>
+          <a href='https://www.heroku.com/home'>Heroku - Deploy and run your apps</a>
+        </li>
+        <li>
+          <a href='http://openweathermap.org/'>OpenWeatherMap - Weather forecasts</a>
+        </li>
+      </ul>
     </div>
 
   );
